087c78d2ecedcf03685f8f838679e05025d23635
Deekshant <deekshant.joshi@gmail.com>
2020-04-26 22:51:13 +0530

    [Feature] Added Hindi translations

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -1,6 +1,12 @@
 <?php
+/**
+ * @copyright Copyright (c) 2020 TrulyMadly Matchmakers Pvt. Ltd. (https://github.com/thetrulymadly)
+ *
+ * @author    Deekshant Joshi (deekshant.joshi@gmail.com)
+ * @since     26 April 2020
+ */
 
-return array (
-  'failed' => 'These credentials do not match our records.',
-  'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-);
+return [
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+];
