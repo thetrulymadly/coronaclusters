60847cda8f469836623b6e53c84fd6237c8f6888
Deekshant <deekshant.joshi@gmail.com>
2020-04-29 17:59:39 +0530

    [Update] Cleanup - auth code

--- resources/lang/hi/places.php
+++ resources/lang/hi/places.php
@@ -3,10 +3,9 @@
  * @copyright Copyright (c) 2020 TrulyMadly Matchmakers Pvt. Ltd. (https://github.com/thetrulymadly)
  *
  * @author    Deekshant Joshi (deekshant.joshi@gmail.com)
- * @since     26 April 2020
+ * @since     29 April 2020
  */
 
 return [
-    'next' => 'Next &raquo;',
-    'previous' => '&laquo; Previous',
+    'india' => 'भारत',
 ];
--- resources/views/layouts/home_layout.blade.php
+++ resources/views/layouts/home_layout.blade.php
@@ -3,12 +3,7 @@
 <head>
     <link href="{{ mix('css/vendor.css') }}" rel="stylesheet">
     <link href="{{ mix('css/covid_'.config('corona.theme').'.css') }}" rel="stylesheet">
-    <link rel="stylesheet" type="text/css"
-          href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.20/b-1.6.1/b-colvis-1.6.1/b-flash-1.6.1/b-html5-1.6.1/b-print-1.6.1/cr-1.5.2/fc-3.3.0/fh-3.1.6/kt-2.5.1/r-2.2.3/rg-1.1.1/rr-1.2.6/sc-2.0.1/sp-1.0.1/datatables.min.css"/>
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link rel="canonical" href="{{ request()->get('canonicalUrl') ?? $url }}"/>
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <meta name="robot" content="all">
 
     <title>{{ $title }}</title>
