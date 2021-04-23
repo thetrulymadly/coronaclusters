5e6e71b2a3c577cad73d2d34e87f3686c21ae2c0
Deekshant <deekshant.joshi@gmail.com>
2021-04-23 23:13:58 +0530

    [Fixes] Plasma: List table made responsive

--- resources/views/components/plasma/donor_requester_list.blade.php
+++ resources/views/components/plasma/donor_requester_list.blade.php
@@ -8,8 +8,10 @@
             @endif
         </h4>
         <div class="float-right d-none d-md-block">
-            <a href="{{ config('app.url').'plasma/request' }}" class="btn btn-secondary mr-3">{{ __('plasma.requests') }} <i class="fa fas fa-ambulance"></i></a>
-            <a href="{{ config('app.url').'plasma/donate' }}" class="btn btn-primary">{{ __('plasma.donors') }} <i class="fa fas fa-heartbeat"></i></a>
+            <a href="{{ config('app.url').'plasma/request' }}" class="btn btn-secondary mr-3">{{ __('plasma.request') }}
+                <i class="fa fas fa-ambulance"></i></a>
+            <a href="{{ config('app.url').'plasma/donate' }}" class="btn btn-primary">{{ __('plasma.donate') }} <i
+                    class="fa fas fa-heartbeat"></i></a>
         </div>
     </div>
     <div class="card-body">
@@ -20,42 +22,44 @@
                 <p>There are no donors for plasma yet. We will update when someone is available to donate</p>
             @endif
         @else
-            <table class="table table-striped table-hover table-sm">
-                <thead>
-                <tr>
-                    <th>Location</th>
-                    <th>Donor</th>
-                    <th>Blood Group</th>
-                    @if(isset($detailed) && $detailed === true)
-                        <th>Phone number</th>
-                        <th>Date of positive</th>
-                        <th>Date of negative</th>
-                    @endif
-                    @if(isset($requesters) && $requesters === true)
-                        <th>Hospital</th>
-                    @endif
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($donors as $donor)
+            <div class="table-responsive-sm">
+                <table class="table table-striped table-hover table-sm">
+                    <thead>
                     <tr>
-                        <td>{{ $donor->geoCity->name . ', ' . $donor->geoState->name }}</td>
-                        <td>{{ ucfirst($donor->gender) }} / {{ $donor->age }}</td>
-                        <td>{{ $donor->blood_group }}</td>
-
+                        <th>Location</th>
+                        <th>Donor</th>
+                        <th>Blood Group</th>
                         @if(isset($detailed) && $detailed === true)
-                            <th>{{ $donor->phone_number }}</th>
-                            <th>{{ $donor->date_of_positive }}</th>
-                            <th>{{ $donor->date_of_negative }}</th>
+                            <th>Phone number</th>
+                            <th>Date of positive</th>
+                            <th>Date of negative</th>
                         @endif
-
                         @if(isset($requesters) && $requesters === true)
-                            <th>{{ $donor->hospital }}</th>
+                            <th>Hospital</th>
                         @endif
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    @foreach($donors as $donor)
+                        <tr>
+                            <td>{{ $donor->geoCity->name . ', ' . $donor->geoState->name }}</td>
+                            <td>{{ ucfirst($donor->gender) }} / {{ $donor->age }}</td>
+                            <td>{{ $donor->blood_group }}</td>
+
+                            @if(isset($detailed) && $detailed === true)
+                                <th>{{ $donor->phone_number }}</th>
+                                <th>{{ $donor->date_of_positive }}</th>
+                                <th>{{ $donor->date_of_negative }}</th>
+                            @endif
+
+                            @if(isset($requesters) && $requesters === true)
+                                <th>{{ $donor->hospital }}</th>
+                            @endif
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         @endif
     </div>
 </div>
